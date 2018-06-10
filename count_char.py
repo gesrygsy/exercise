d6159862a62f2ef810d068dc723aaf1a41ed925c
gesrygsy <gesrygsy@yahoo.co.jp>
2018-06-10 22:12:09 +0800

    count character

---
+++ count_char.py
@@ -0,0 +1,17 @@
+def count_characters():
+     count_dict = {}
+     string = input("Input a character:")
+     for c in string:
+         if c in count_dict:
+             count_dict[c] += 1
+         else:
+             count_dict[c] = 1
+
+     print(count_dict)
+
+count_characters()
+
+
+from collections import Counter
+
+print(Counter(input("Inputer a char:")))
